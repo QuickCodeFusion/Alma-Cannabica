74c0be4dbdb7576ab55102fc1e18b3fb0597acf8
Uriel Campos <urielcampos448@gmail.com>
2023-10-20 21:28:43 -0300

    Initial config

---
+++ alma-cannabica/firebase/config.ts
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDyQL6ZLOr1Z8o0hoD4sj_IiTCaPKRDpac",
+  authDomain: "alma-cannabica.firebaseapp.com",
+  projectId: "alma-cannabica",
+  storageBucket: "alma-cannabica.appspot.com",
+  messagingSenderId: "1050524985544",
+  appId: "1:1050524985544:web:147c55fd5ca0567aab1dfd",
+  measurementId: "G-69GK8J943M"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
