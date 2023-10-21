35cc8900073f7354ec93a2134f18a8dab704bd4c
Juan Diaz <juanjoc2000@hotmail.com>
2023-10-21 20:30:47 -0300

    feat[NavBar]: Created navBar

---
+++ alma-cannabica/components/navBar/NavBar.tsx
@@ -0,0 +1,12 @@
+'use client';
+import SearchBar from "./searchBar/SearchBar";
+
+const NavBar = () => {
+    return (
+        <div>
+            <div>
+                <SearchBar/>
+            </div>
+        </div>
+    )
+}
