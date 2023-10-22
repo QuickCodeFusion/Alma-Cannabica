43ff21c21b65c6d74c5fe4dc55b979e21d7ea7fa
Juan Diaz <juanjoc2000@hotmail.com>
2023-10-22 20:56:18 -0300

    Feat[Menu]: Added functionalities

---
+++ alma-cannabica/components/navBar/menu/menuOpen/MenuOpen.tsx
@@ -0,0 +1,19 @@
+
+const MenuOpen = () => {
+
+    return(
+        <div>
+            <div>
+                <h1>Alma Cannabica</h1>
+                <p>Login</p>
+                <p>Register</p>
+                <p>Compras</p>
+                <p>Historial</p>
+                <p>Perfil</p>
+                <p>About</p>
+            </div>
+        </div>
+    )
+}
+
+export default MenuOpen
