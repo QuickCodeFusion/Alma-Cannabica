1042b9c54805a10fbd1307e8ffdff578b08b3e63
Juan Diaz <juanjoc2000@hotmail.com>
2023-10-22 20:56:43 -0300

    feat[Navbar]: Added searchBar and Menu

--- alma-cannabica/components/navBar/NavBar.tsx
+++ alma-cannabica/components/navBar/NavBar.tsx
@@ -1,11 +1,14 @@
 'use client';
 import SearchBar from "./searchBar/SearchBar";
+import Menu from "./menu/Menu";
 
 const NavBar = () => {
     return (
         <div>
-            <div>
+            <div >
+                <h1>Alma Cannabica</h1>
                 <SearchBar/>
+                <Menu/>
             </div>
         </div>
     )
