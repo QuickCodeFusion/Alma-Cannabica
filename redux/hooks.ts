52a81c57d2128e3537e01353b8cca96401119d8f
juanpo12 <juanjoc2000@hotmail.com>
2023-10-23 15:02:51 -0300

    config hooks

---
+++ redux/hooks.ts
@@ -0,0 +1,10 @@
+// Se importan los hooks de redux con nombres diferentes de los de react.
+import {type TypedUseSelectorHook, useSelector as useReduxSelector, useDispatch as useReduxDispatch} from 'react-redux';
+import {type store} from './store';
+
+// Se le asignas los tipos de ReduxDispatch y de ReduxState.
+// Es importante que de apartir de ahora siempre se importen de aca.
+export const useDispatch = () => useReduxDispatch<ReduxDispatch>();
+export type ReduxDispatch = typeof store.dispatch;
+export const useSelector: TypedUseSelectorHook<ReduxState> = useReduxSelector;
+export type ReduxState = ReturnType<typeof store.getState>;
