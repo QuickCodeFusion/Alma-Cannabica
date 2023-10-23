b69dd5645ef2d9818a1633aeba7b69ed05f71446
Uriel Campos <urielcampos448@gmail.com>
2023-10-22 22:35:05 -0300

    fix[firebase-admin] config typo

--- alma-cannabica/firebase/admin-config.ts
+++ alma-cannabica/firebase/admin-config.ts
@@ -1,12 +1,11 @@
-import { initializeApp } from "firebase-admin";
-import { cert, getApp, getApps } from "firebase-admin/app";
-import { getAuth } from "firebase-admin/auth";
+import { initializeApp, getApps, cert, getApp } from 'firebase-admin/app';
+import { getAuth } from 'firebase-admin/auth';
 
 const firebaseAdminConfig = {
     credential: cert({
         projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+        privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, "\n"),
     })
 }
 
