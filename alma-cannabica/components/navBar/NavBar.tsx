3e7159fbe4154aebfa9754ff36a611b2e76ec55b
Juan Diaz <juanjoc2000@hotmail.com>
2023-10-23 10:46:24 -0300

    feat[Navbar]

--- alma-cannabica/components/navBar/NavBar.tsx
+++ alma-cannabica/components/navBar/NavBar.tsx
@@ -1,10 +1,11 @@
 'use client';
 import SearchBar from "./searchBar/SearchBar";
 import Menu from "./menu/Menu";
+import style from './navbar.module.css'
 
 const NavBar = () => {
     return (
-        <div>
+        <div className={style.navbar}>
             <div >
                 <h1>Alma Cannabica</h1>
                 <SearchBar/>
