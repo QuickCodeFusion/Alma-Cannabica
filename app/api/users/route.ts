690e956a4b17a55d5a2a35c16d18d2bf8802aec8
juanpo12 <juanjoc2000@hotmail.com>
2023-10-25 19:47:39 -0300

    feat[PUT]: Created update user

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,6 +1,8 @@
-import { doc, getDoc } from 'firebase/firestore'
+import { doc, getDoc, updateDoc } from 'firebase/firestore'
 import { db } from '@/firebase/config'
 import { type NextRequest, NextResponse } from 'next/server'
+import { cookies } from 'next/headers'
+import { auth } from '@/firebase/admin-config'
 
 export const GET = async (req: NextRequest): Promise<NextResponse> => {
 	try {
@@ -9,6 +11,29 @@ export const GET = async (req: NextRequest): Promise<NextResponse> => {
 		const userSnapshot = await getDoc(userRef)
 		const user = userSnapshot.data()
 		return NextResponse.json(user, { status: 200 })
+	} catch (error: any) {
+		return NextResponse.json({ error: error.message }, { status: 400 })
+	}
+}
+
+export const PUT = async (req: NextRequest): Promise<NextResponse> => {
+	try {
+		const sessionToken = cookies().get('session')?.value ?? ''
+
+		if (!sessionToken) {
+			throw new Error('User must be logged in')
+		}
+
+		const { uid } = await auth.verifySessionCookie(sessionToken, true)
+
+		const { name, email, photoUrl, addresses } = await req.json()
+
+		const userRef = doc(db, 'users', uid)
+		name && (await updateDoc(userRef, { name }))
+		email && (await updateDoc(userRef, { email }))
+		photoUrl && (await updateDoc(userRef, { photoUrl }))
+		addresses && (await updateDoc(userRef, { addresses }))
+		return NextResponse.json({ message: 'User updated' }, { status: 200 })
 	} catch (error: any) {
 		return NextResponse.json({ error: error.message }, { status: 400 })
 	}
