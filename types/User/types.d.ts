97c27ceb068f7ff7fc22a59ca8dfa32993c5dbe3
Uriel Campos <urielcampos448@gmail.com>
2023-10-25 14:13:27 -0300

    Add User type

--- types/User/types.d.ts
+++ types/User/types.d.ts
@@ -1,9 +1,21 @@
 export interface Address {
-  id: string
-  name: string
-  address: string
-  city: string
-  province: string
-  zipCode: string
-  country: string
+	id: string
+	name: string
+	address: string
+	city: string
+	province: string
+	zipCode: string
+	country: string
+}
+
+export interface User {
+	uid: string
+	name: string
+	email: string
+	photoUrl: string
+	addresses: Address[]
+}
+
+export interface UsersList {
+	// TODO: Add type for users list
 }
