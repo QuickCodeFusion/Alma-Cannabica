364a49277865173187c74ba996b352996dffe299
Uriel Campos <urielcampos448@gmail.com>
2023-10-25 14:11:07 -0300

    feat[Dashboard]: add admin user validation

--- components/admin/Dashboard.tsx
+++ components/admin/Dashboard.tsx
@@ -1,14 +1,30 @@
-import Users from "./Users"
-import Sidebar from "./Sidebar"
+import Users from './Users'
+import Sidebar from './Sidebar'
 
 const Dashboard = (): React.JSX.Element => {
-	return (
-		<div>
-			<h1>Admin Dashboard</h1>
-            <Sidebar/>
-            <Users/>
-		</div>
-	)
+	const user = {
+		uid: '123',
+		email: 'Q4U5C@example.com',
+		customClaims: {
+			admin: true
+		}
+	}
+
+	if (user?.customClaims?.admin) {
+		return (
+			<div>
+				<h1>Admin Dashboard</h1>
+				<Sidebar/>
+				<Users/>
+			</div>
+		)
+	} else {
+		return (
+			<div>
+				{'NO SE PUEDE PASAR PAPA'}
+			</div>
+		)
+	}
 }
 
 export default Dashboard
