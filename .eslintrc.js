7f83ce51316abdfa2e9147af046cbf8bed406ad6
UrielC2004 <131821174+UFCampos@users.noreply.github.com>
2023-10-25 10:34:16 -0300

    Refactor: Added JSDocs and types (#4)
    * feat[Navbar]
    
    * feat[Store]:Created store
    
    * config hooks
    
    * feat[Page CreateProducts]: Created form Products
    
    * feat[createProducts]: Created components CreateProducts
    
    * feat[productsApi]:Created productsAPI
    
    * feat[providers]: config store and provider
    
    * feat[POSTproduct]: Created route postProducts
    
    * refactor: Add EsLint rules
    
    * config
    
    * refactor[GET and Post]: DELETED
    
    * feat[products]: merge get and post
    
    * uninstall prettier
    
    * feat[editProduct Controller and PUT router]
    
    * updated eslint rules
    
    * update eslint config
    
    * update tsconfig to include eslintrc.js
    
    * refactor[createUser controller]: add Address type
    
    * update eslint rules to tab identation
    
    * add JSDocs to authUtils & fix type error
    
    * Add JSDoc to uploadFile utils
    
    ---------
    
    Co-authored-by: juanpo12 <juanjoc2000@hotmail.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,54 +1,38 @@
 module.exports = {
-	"env": {
-		"browser": true,
-		"es2021": true,
-		"node": true
+	env: {
+		browser: true,
+		es2021: true,
+		node: true
 	},
-	"extends": [
-		"eslint:recommended",
-		"plugin:@typescript-eslint/recommended",
-		"plugin:react/recommended",
-		"prettier"
+	extends: [
+		'standard-with-typescript',
+		'plugin:react/recommended'
 	],
-	"overrides": [
+	overrides: [
 		{
-			"env": {
-				"node": true
+			env: {
+				node: true
 			},
-			"files": [
-				".eslintrc.{js,cjs}"
+			files: [
+				'.eslintrc.js'
 			],
-			"parserOptions": {
-				"sourceType": "script"
+			parserOptions: {
+				sourceType: 'script'
 			}
 		}
 	],
-	"parser": "@typescript-eslint/parser",
-	"parserOptions": {
-		"ecmaVersion": "latest",
-		"sourceType": "module"
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module'
 	},
-	"plugins": [
-		"@typescript-eslint",
-		"react"
+	plugins: [
+		'react'
 	],
-	"rules": {
-		"indent": [
-			"error",
-			"tab"
-		],
-		"linebreak-style": [
-			"error",
-			"unix"
-		],
-		"quotes": [
-			"error",
-			"double"
-		],
-		"semi": [
-			"error",
-			"always"
-		],
-		"react/react-in-jsx-scope": "off",
+	rules: {
+		'react/react-in-jsx-scope': 'off',
+		'@typescript-eslint/no-explicit-any': 'off',
+		'@typescript-eslint/strict-boolean-expressions': 'off',
+		'@typescript-eslint/indent': ['error', 'tab'],
+		'no-tabs': 'off'
 	}
-};
+}
