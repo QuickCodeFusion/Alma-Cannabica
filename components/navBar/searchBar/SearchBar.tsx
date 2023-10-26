eac6c2e6ae8b04269ed09844ab7324735024b05f
Juan José Diaz <123817829+juanpo12@users.noreply.github.com>
2023-10-26 20:31:57 -0300

    Navbar2 (#10)
    * feat[Navbar]
    
    * feat[style]:Change color navbar
    
    * style navbar
    
    * feat[style]: Added styles and buttons

--- components/navBar/searchBar/SearchBar.tsx
+++ components/navBar/searchBar/SearchBar.tsx
@@ -1,13 +1,15 @@
-"use client";
+'use client'
+import style from './searchBar.module.css'
+import Image from 'next/image';
 
 const SearchBar = () => {
     
-	return (
-		<div>
-			<input type="text" onChange={(e) => console.log(e.target.value)}/>
-			<button>Search</button>
-		</div>
-	);
-};
+    return (
+        <div className={style.searchBar}>
+            <input type="text" onChange={(e) => console.log(e.target.value)} className={style.input}/>
+            <Image src="/buscar.png" alt="Buscar" width={20} height={20}></Image>
+        </div>
+    )
+}
 
 export default SearchBar;
