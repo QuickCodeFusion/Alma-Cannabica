b9343d759952d5e4318d3b5c41fa0b8f8594e692
juanpo12 <juanjoc2000@hotmail.com>
2023-10-26 18:58:01 -0300

    feat[style]:Change color navbar

--- components/navBar/searchBar/SearchBar.tsx
+++ components/navBar/searchBar/SearchBar.tsx
@@ -1,10 +1,11 @@
 'use client'
+import { style } from './searchBar.module.css'
 
 const SearchBar = () => {
     
     return (
         <div>
-            <input type="text" onChange={(e) => console.log(e.target.value)}/>
+            <input type="text" onChange={(e) => console.log(e.target.value)} className={style.input}/>
             <button>Search</button>
         </div>
     )
