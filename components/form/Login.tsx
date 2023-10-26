d5afa3aa40034bb513d3f53b0d57108820a62f67
Uriel Campos <urielcampos448@gmail.com>
2023-10-26 20:46:06 -0300

    refactor Login component to follow eslint rules

--- components/form/Login.tsx
+++ components/form/Login.tsx
@@ -1,29 +1,31 @@
-"use client";
+'use client'
+import { login } from '@/utils/authUtils'
+import { useState } from 'react'
+import SubmitButton from '@/components/button/submitButton'
 
-import { login } from "@/utils/authUtils";
-import { useState } from "react";
-import SubmitButton from "@/components/button/submitButton";
-
-const Login = () => {
+const Login = (): React.JSX.Element => {
 	const [form, setForm] = useState({
-		email: "",
-		password: "",
-	});
+		email: '',
+		password: ''
+	})
 
 	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
 		setForm({
 			...form,
-			[e.target.name]: e.target.value,
-		});
-	};
+			[e.target.name]: e.target.value
+		})
+	}
 
 	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-		e.preventDefault();
+		e.preventDefault()
 		login(form, setForm)
-			.finally(() => {
-				alert("Has iniciado sesión exitosamente");
-			});
-	};
+			.then(() => {
+				alert('Has iniciado sesión exitosamente')
+			})
+			.catch((error) => {
+				console.error(error)
+			})
+	}
 
 	return (
 		<form method="POST" onSubmit={handleSubmit}>
@@ -31,7 +33,7 @@ const Login = () => {
 			<input type="password" name="password" value={form.password} onChange={handleChange} placeholder="Contraseña" />
 			<SubmitButton title="Login"></SubmitButton>
 		</form>
-	);
-};
+	)
+}
 
-export default Login;
+export default Login
