2578944f1c98b741f0f45551345da083179cb074
juanpo12 <juanjoc2000@hotmail.com>
2023-10-26 09:48:37 -0300

    feat[createAddresses]

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -3,6 +3,7 @@ import { db } from '@/firebase/config'
 import { type NextRequest, NextResponse } from 'next/server'
 import { cookies } from 'next/headers'
 import { auth } from '@/firebase/admin-config'
+import { updateAddress } from './updateAddresses'
 
 export const GET = async (req: NextRequest): Promise<NextResponse> => {
 	try {
@@ -26,13 +27,17 @@ export const PUT = async (req: NextRequest): Promise<NextResponse> => {
 
 		const { uid } = await auth.verifySessionCookie(sessionToken, true)
 
-		const { name, email, photoUrl, addresses } = await req.json()
+		const { name, email, photoUrl, address, nameAddress } = await req.json()
 
 		const userRef = doc(db, 'users', uid)
 		name && (await updateDoc(userRef, { name }))
 		email && (await updateDoc(userRef, { email }))
 		photoUrl && (await updateDoc(userRef, { photoUrl }))
-		addresses && (await updateDoc(userRef, { addresses }))
+
+		if (address && nameAddress) {
+			await updateAddress(uid, address, nameAddress)
+		}
+
 		return NextResponse.json({ message: 'User updated' }, { status: 200 })
 	} catch (error: any) {
 		return NextResponse.json({ error: error.message }, { status: 400 })
