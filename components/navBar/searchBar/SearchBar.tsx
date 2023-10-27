068d847644f629d07367a810f63dd225acb2e95f
juanpo12 <juanjoc2000@hotmail.com>
2023-10-26 21:50:29 -0300

    styles[landing and footer]

--- components/navBar/searchBar/SearchBar.tsx
+++ components/navBar/searchBar/SearchBar.tsx
@@ -3,11 +3,15 @@ import style from './searchBar.module.css'
 import Image from 'next/image';
 
 const SearchBar = () => {
-    
+
     return (
         <div className={style.searchBar}>
-            <input type="text" onChange={(e) => console.log(e.target.value)} className={style.input}/>
-            <Image src="/buscar.png" alt="Buscar" width={20} height={20}></Image>
+            <div>
+                <input type="text" onChange={(e) => console.log(e.target.value)} className={style.input} />
+            </div>
+            <div className={style.button}>
+                <Image src="/buscar.png" alt="Buscar" width={20} height={20}></Image>
+            </div>
         </div>
     )
 }
