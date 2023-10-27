b54d4f8b110b57645977d980a0b7b6a913dc4481
Juan José Diaz <123817829+juanpo12@users.noreply.github.com>
2023-10-26 21:50:59 -0300

    Landing page (#11)
    * feat[Navbar]
    
    * feat[style]:Change color navbar
    
    * style navbar
    
    * feat[style]: Added styles and buttons
    
    * styles[landing and footer]

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
-import type { Metadata } from 'next'
-import './globals.css'
-import { Providers } from './providers'
-import NavBar from '@/components/navBar/NavBar'
+import type { Metadata } from "next";
+import "./globals.css";
+import { Providers } from "./providers";
+import NavBar from "@/components/navBar/NavBar";
+import Footer from "@/components/footer/Footer";
 
 export const metadata: Metadata = {
 	title: 'Alma Cannabica'
@@ -18,6 +19,7 @@ export default function RootLayout ({
 				<Providers>
 					<NavBar/>
 					{children}
+					<Footer/>
 				</Providers>
 			</body>
 		</html>
