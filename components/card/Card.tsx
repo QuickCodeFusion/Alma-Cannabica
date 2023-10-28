d7e378c47512217e8c1615030c794ca0f9ad46cc
juanpo12 <juanjoc2000@hotmail.com>
2023-10-27 22:51:45 -0300

    refactor eslint

--- components/card/Card.tsx
+++ components/card/Card.tsx
@@ -1,19 +1,19 @@
-import { Image } from "@nextui-org/react"
+import { Image } from '@nextui-org/react'
 
-const Card = ({product}) => {
-    const {name, price, description, image, category} = product
-    
-    return (
-        <div>
-            <div>
-                <Image src="logo.png"/>
-                <h1>{name}</h1>
-                <p>{price}</p>
-                <p>{category}</p>
-            </div>
+const Card = ({ product }) => {
+	const { name, price, description, image, category } = product
 
-        </div>
-    )
+	return (
+		<div>
+			<div>
+				<Image src="logo.png"/>
+				<h1>{name}</h1>
+				<p>{price}</p>
+				<p>{category}</p>
+			</div>
+
+		</div>
+	)
 }
 
 export default Card
