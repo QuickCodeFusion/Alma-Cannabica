7f47b327e9b8211dea3399472c57d6b7301ea32e
juanpo12 <juanjoc2000@hotmail.com>
2023-10-27 21:19:25 -0300

    feat[cart]: Added style

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,18 @@
-import type { Metadata } from "next";
-import "./globals.css";
-import { Toaster } from "sonner";
-import { Providers } from "./providers";
-import NavBar from "@/components/navBar/NavBar";
-import Footer from "@/components/footer/Footer";
-import Cart from "@/components/cart/Cart";
+import type { Metadata } from 'next'
+import './globals.css'
+import { Toaster } from 'sonner'
+import { Providers } from './providers'
+import NavBar from '@/components/navBar/NavBar'
+import Footer from '@/components/footer/Footer'
 
 export const metadata: Metadata = {
-	title: "Alma Cannabica",
-};
+	title: 'Alma Cannabica'
+}
 
-export default function RootLayout({
-	children,
+export default function RootLayout ({
+	children
 }: {
-  children: React.ReactNode
+	children: React.ReactNode
 }) {
 	return (
 		<html lang="en" className='dark'>
@@ -21,11 +20,10 @@ export default function RootLayout({
 				<Providers>
 					<Toaster richColors position="top-center" />
 					<NavBar/>
-					<Cart/>
 					{children}
 					<Footer/>
 				</Providers>
 			</body>
 		</html>
-	);
+	)
 }
--- components/cart/Cart.tsx
+++ components/cart/Cart.tsx
@@ -1,20 +1,23 @@
 'use client'
 import style from './cart.module.css'
 
-const Cart = () => {
-
-    return (
-        <div className={style.container}>
-            <div>
-                <div>
-                    <h1>Cart</h1>
-                </div>
-                <div>
-                    <p>products</p>
-                </div>
-            </div>
-        </div>
-    )
+const Cart = (): JSX.Element => {
+	return (
+		<div>
+			{
+				<div className={style.container}>
+					<div>
+						<div>
+							<h1>Cart</h1>
+						</div>
+						<div>
+							<p>products</p>
+						</div>
+					</div>
+				</div>
+			}
+		</div>
+	)
 }
 
 export default Cart
