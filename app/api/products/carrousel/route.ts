6fe58e016642953e38bfd1c592fe92033231c553
juanpo12 <juanjoc2000@hotmail.com>
2023-10-30 00:44:29 -0300

    Feat[DELETE]: Created route Delete

--- app/api/products/carrousel/route.ts
+++ app/api/products/carrousel/route.ts
@@ -1,5 +1,5 @@
 import { db } from '@/firebase/config'
-import { addDoc, collection, getDocs } from 'firebase/firestore'
+import { addDoc, collection, deleteDoc, doc, getDocs } from 'firebase/firestore'
 import { type NextRequest, NextResponse } from 'next/server'
 
 export const GET = async (): Promise<NextResponse> => {
@@ -25,6 +25,20 @@ export const POST = async (req: NextRequest): Promise<NextResponse> => {
 			image
 		})
 		return NextResponse.json({ message: 'Product added' }, { status: 201 })
+	} catch (error: any) {
+		return NextResponse.json({ error: error.message }, { status: 400 })
+	}
+}
+
+export const DELETE = async (req: NextRequest): Promise<NextResponse> => {
+	try {
+		const { itemId }: { itemId: string } = await req.json()
+
+		const productsRef = doc(db, 'cardCarrousel', itemId)
+
+		await deleteDoc(productsRef)
+
+		return NextResponse.json({ message: 'Product deleted ' + itemId }, { status: 200 })
 	} catch (error: any) {
 		return NextResponse.json({ error: error.message }, { status: 400 })
 	}
