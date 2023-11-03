2e74b87ecb6a5f99fe7f6d5275422270e39a6c3f
Davian Pabon <wilsondavianpa1998@outlook.com>
2023-11-03 16:01:08 -0500

    implementación del carrusel (#22)
    * carousel auto
    
    * carousel style
    
    * update carousel

--- redux/store.ts
+++ redux/store.ts
@@ -2,16 +2,20 @@ import { configureStore } from '@reduxjs/toolkit'
 import { setupListeners } from '@reduxjs/toolkit/query'
 import { adminAPI } from './service/adminAPI'
 import { productsAPI } from './service/productsAPI'
+import { carouselAPI } from './service/carouselAPI'
+
 
 export const store = configureStore({
 	reducer: {
 		[adminAPI.reducerPath]: adminAPI.reducer,
-		[productsAPI.reducerPath]: productsAPI.reducer
+		[productsAPI.reducerPath]: productsAPI.reducer,
+		[carouselAPI.reducerPath]: carouselAPI.reducer
 	},
 	middleware: getDefaultMiddleware =>
 		getDefaultMiddleware().concat(
 			adminAPI.middleware,
-			productsAPI.middleware
+			productsAPI.middleware,
+			carouselAPI.middleware
 		)
 })
 
