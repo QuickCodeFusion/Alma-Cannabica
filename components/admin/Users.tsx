0b98018d8026eaf1b4816eb7f5b34bdf38731f2e
Uriel Campos <urielcampos448@gmail.com>
2023-11-06 15:39:51 -0300

    feat[users FC] add loading & error handling modularized users table to its own component

--- components/admin/Users.tsx
+++ components/admin/Users.tsx
@@ -2,15 +2,12 @@
 import { useEffect, useState } from 'react'
 import { toast } from 'sonner'
 import { useSetAdminMutation, useDisableUserMutation, useGetAllAuthUsersQuery } from '@/redux/service/adminAPI'
-import { type UserRecord } from 'firebase-admin/auth'
-import { Button } from '@nextui-org/react'
-import UserButton from '../button/admin/userButton'
 
 // You might import your user data or fetch it from an API here
 
 const Users = (): React.JSX.Element => {
 	const [loading, setLoading] = useState(false)
-	const { data, isLoading } = useGetAllAuthUsersQuery(null)
+	const { data, isLoading, isError, error } = useGetAllAuthUsersQuery(null)
 
 	const [postAdmin] = useSetAdminMutation()
 
@@ -87,59 +84,9 @@ const Users = (): React.JSX.Element => {
 
 	return (
 		<div >
-			<table className="min-w-full divide-y divide-gray-200">
-				<thead className="bg-gray-950">
-					<tr>
-						<th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                            UID
-						</th>
-						<th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                            Email
-						</th>
-						<th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                            Rango
-						</th>
-					</tr>
-				</thead>
-				<tbody className="bg-gray-950 divide-y divide-gray-200">
-					{data?.users?.map((user: UserRecord) => (
-						<tr key={user.uid}>
-							<td className="px-6 py-4 whitespace-nowrap">
-								{user.uid}
-							</td>
-							<td className="px-6 py-4 whitespace-nowrap">
-								{user.email}
-							</td>
-							<td className="px-6 py-4 whitespace-nowrap">
-								{user.customClaims?.admin ? 'Admin' : 'User'}
-							</td>
-							<td className="px-6 py-4 whitespace-nowrap">
-								{
-									loading
-										? <Button radius='full' isLoading>Cargando</Button>
-										:										(
-											user.disabled
-												? <UserButton title='Habilitar' txtColor='green' btnColor='success' action={() => handleUnban(user.uid)}/>
-												: <UserButton title='Deshabilitar' txtColor='red' btnColor='danger' action={() => handleBan(user.uid)}/>
-										)
-								}
-							</td>
-							<td className="px-6 py-4 whitespace-nowrap">
-								{
-									loading
-										? <Button radius='full' isLoading>Cargando</Button>
-										:										(
-											user.customClaims?.admin
-												? <UserButton title='Quitar Admin' txtColor='yellow' btnColor='warning' action={() => handleRemoveAdmin(user.uid)}/>
-												: <UserButton title='Otorgar Admin' txtColor='blue' btnColor='primary' action={() => handleGiveAdmin(user.uid)}/>
-										)
-								}
-							</td>
-						</tr>
-					))}
-				</tbody>
-			</table>
+			
 			{isLoading && <div>Loading...</div>}
+			{isError && <div>Ups! Ha habido un error al cargar la página: {JSON.stringify(error?.data)}</div>}
 		</div>
 	)
 }
