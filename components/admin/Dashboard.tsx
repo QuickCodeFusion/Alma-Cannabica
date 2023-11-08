a6e801c436e1cbc010b63a2c664e2c639ac09388
Uriel Campos <urielcampos448@gmail.com>
2023-11-08 03:30:53 -0300

    feat[dashboard FC]: add loading spinner

--- components/admin/Dashboard.tsx
+++ components/admin/Dashboard.tsx
@@ -5,6 +5,7 @@ import { useUserSession } from '@/app/userContext'
 import { useEffect, useState } from 'react'
 import { auth } from '@/firebase/config'
 import { toast } from 'sonner'
+import { Spinner } from '@nextui-org/react'
 
 const Dashboard = (): React.JSX.Element => {
 	const { userSession: user } = useUserSession()
@@ -21,12 +22,18 @@ const Dashboard = (): React.JSX.Element => {
 			.catch((error) => {
 				toast.error('Something went wrong: ' + error)
 			})
-	}, [])
+	}, [user])
 
 	if (loading) {
 		return (
-			<div>
-				<h1>Loading...</h1>
+			<div className='flex items-center justify-center'>
+				<Spinner
+					label="Verificando credenciales..."
+					labelColor="success"
+					color="success"
+					aria-label="Loading Spinner"
+					size="lg"
+				/>
 			</div>
 		)
 	}
