604a5449771516e32e9ddeac91ce086acfcf5f2c
altair201 <wilsondavianpa1998@outlook.com>
2023-11-09 16:14:38 -0500

    card products

--- components/card/Card.tsx
+++ components/card/Card.tsx
@@ -21,11 +21,14 @@ const CardAricule = ({ product }: { product: CardProduct }): React.JSX.Element =
 							<Button isIconOnly color="success" variant="bordered" >
 								+
 							</Button>
-							<Button radius="full" className="bg-gradient-to-tr from-pink-500 to-yellow-500 text-white">
-								Button
+							<Button  className="bg-gradient-to-tr from-green-500 to-blue-500 text-white mt-1">
+								COMPRAR
 							</Button>
 						</div>
-						<p className="text-600">${price}</p>
+						<div className='flex-col direction-colum h-20'>
+						<p className="text-600">{category}</p>
+						<p className="text-600 mt-5">${price}</p>
+						</div>
 					</div>
 				</CardFooter>
 			</Card>
