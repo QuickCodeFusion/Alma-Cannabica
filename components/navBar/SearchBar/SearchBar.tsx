77c7d5fac810d685ea8a20fdc36234e8d45a1d93
juanpo12 <juanjoc2000@hotmail.com>
2023-11-15 16:31:17 -0300

    feat[searchbar]:Added search for name

--- components/navBar/SearchBar/SearchBar.tsx
+++ components/navBar/SearchBar/SearchBar.tsx
@@ -3,12 +3,23 @@ import style from './SearchBar.module.css'
 import Image from 'next/image'
 import { search } from '@/redux/feature/searchBarSlice'
 import { useDispatch } from '@/redux/hooks'
+import { loadProducts } from '@/redux/feature/productsSlice'
+import { useGetFiltersQuery } from '@/redux/service/productsFilterAPI'
+import { useState } from 'react'
 
 const SearchBar = (): React.JSX.Element => {
 	const dispatch = useDispatch()
+	const [nameProduct, setNameProduct] = useState('')
+
+	const { data: products } = useGetFiltersQuery({ nameProduct })
+
 	const handleChange = (event: React.ChangeEvent<HTMLInputElement>): any => {
 		const value = event.target.value
-		dispatch(search(value))
+		setNameProduct(value)
+	}
+	const handleSubmit = (): any => {
+		dispatch(search(nameProduct))
+		dispatch(loadProducts(products))
 	}
 
 	return (
@@ -17,7 +28,9 @@ const SearchBar = (): React.JSX.Element => {
 				<input type="text" onChange={handleChange} className={style.input} />
 			</div>
 			<div className="hidden sm:flex">
-				<Image src="/buscar.png" alt="Buscar" width={20} height={20}></Image>
+				<button className={style.button} onClick={handleSubmit}>
+					<Image src="/buscar.png" alt="Buscar" width={20} height={20}></Image>
+				</button>
 			</div>
 		</div>
 	)
