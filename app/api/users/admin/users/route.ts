ec1b1f8368d5cd6ed0645ce8f18b03d4e286fbf5
Uriel Campos <urielcampos448@gmail.com>
2023-11-15 01:12:40 -0300

    feat moved listusers func to api route

--- app/api/users/admin/users/route.ts
+++ app/api/users/admin/users/route.ts
@@ -1,7 +1,16 @@
-import { listUsers } from '@/utils/usersUtils'
+import { auth } from '@/firebase/admin-config'
 import { NextResponse } from 'next/server'
 
 export const GET = async (): Promise<NextResponse> => {
+	const listUsers = async (nextPageToken?: string): Promise<any> => {
+		const listUsersResult = await auth.listUsers()
+		const users = listUsersResult.users.map((userRecord) => userRecord.toJSON())
+		const result = {
+			users: [...users],
+			nextPageToken: listUsersResult.pageToken
+		}
+		return result
+	}
 	const usersList = await listUsers()
 
 	return NextResponse.json(usersList, { status: 200 })
