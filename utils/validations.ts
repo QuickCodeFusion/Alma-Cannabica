ddf4acb35702f162d9462d2da1074ec91a82baca
Uriel Campos <urielcampos448@gmail.com>
2023-11-15 19:56:13 -0300

    feat[utils]: add validations file

---
+++ utils/validations.ts
@@ -0,0 +1,34 @@
+import { type Dispatch } from 'react'
+
+/**
+ * Validates the product information and updates the disabled state.
+ *
+ * @param {Object} product - The product information to validate.
+ * @param {string} product.name - The name of the product.
+ * @param {File} product.image - The image file of the product.
+ * @param {string} product.description - The description of the product.
+ * @param {string} product.price - The price of the product.
+ * @param {string} product.category - The category of the product.
+ * @param {Dispatch<boolean>} setIsDisabled - The state setter to update the disabled state.
+ * @return {void} No return value.
+ */
+export const productValidate = ({
+	name,
+	image,
+	description,
+	price,
+	category
+}:
+{ name: string
+	image: File
+	description: string
+	price: string
+	category: string
+},
+setIsDisabled: Dispatch<boolean>): void => {
+	if (!name || !image || !description || !price || !category) {
+		setIsDisabled(true)
+	} else {
+		setIsDisabled(false)
+	}
+}
