b3c4e5fa027166bf5bd6f64288a2f4a7aa1c1b6e
Uriel Campos <urielcampos448@gmail.com>
2023-11-16 18:26:19 -0300

    feat[createproduct]: categories select known bugs: - Categories duplicate - Select empty category

--- components/form/CreateProduct.tsx
+++ components/form/CreateProduct.tsx
@@ -6,36 +6,46 @@ import { useEffect, useState } from 'react'
 import { Input } from '@nextui-org/react'
 import { uploadFile } from '@/utils/uploadFile'
 import { productValidate } from '@/utils/validations'
+import CategorySelect from './CategorySelect'
+
+export interface productState {
+	name: string
+	description: string
+	price: string
+	image: File | undefined
+	category: string[]
+}
 
 const CreateProduct = (): React.JSX.Element => {
-	const fields = [{ name: 'name', label: 'Nombre' }, { name: 'description', label: 'Descripción' }, { name: 'price', label: 'Precio' }, { name: 'category', label: 'Categoría' }]
 	const [isDisabled, setIsDisabled] = useState(true)
-	const [product, setProduct] = useState<{
-		name: string
-		description: string
-		price: string
-		image: File | undefined
-		category: string
-	}>({
+	const [categories, setCategory] = useState<string[]>([])
+	const [categoryInput, setCategoryInput] = useState<string>('')
+	const [product, setProduct] = useState<productState>({
 		name: '',
 		description: '',
 		price: '',
 		image: undefined,
-		category: ''
+		category: []
 	})
 	const [loading, setLoading] = useState(false)
 	const [createProduct] = useCreateProductMutation()
 
 	const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
-		setProduct({
-			...product,
-			[e.target.name]: e.target.value
-		})
-		productValidate(product, setIsDisabled)
-		console.log(isDisabled, product)
+		if (e.target.name === 'image') {
+			setProduct({
+				...product,
+				[e.target.name]: e.target.files?.[0]
+			})
+		} else {
+			setProduct({
+				...product,
+				[e.target.name]: e.target.value
+			})
+		}
 	}
 
 	useEffect(() => {
+		console.log(product)
 		productValidate(product, setIsDisabled)
 	}, [product])
 
@@ -43,7 +53,7 @@ const CreateProduct = (): React.JSX.Element => {
 		setLoading(true)
 		const { name, description, price, image, category } = product
 		if (image === undefined) throw new Error('No se ha subido una imagen')
-		const imageUrl = uploadFile(image, image.name).then((url) => url)
+		const imageUrl = await uploadFile(image, image.name)
 
 		const normalizedProduct = {
 			name,
@@ -53,32 +63,64 @@ const CreateProduct = (): React.JSX.Element => {
 			category
 		}
 		createProduct(normalizedProduct)
-
 			.then(() => {
 				toast.success('Producto creado correctamente')
-				setLoading(false)
 			})
 			.catch((error: any) => {
 				toast.error('Error al crear Producto: ' + error)
+			})
+			.finally(() => {
+				setLoading(false)
 			})
 	}
 
 	return (
 		<div>
-			<h1>Create Product</h1>
+			<h1>Complete la información del producto</h1>
 			<form onSubmit={handleSubmit} method='POST'>
-				{fields.map((field) => (
-					<Input
-						key={field.name}
-						variant='underlined'
-						isRequired
-						label={field.label}
-						placeholder={field.label}
-						name={field.name}
-						value={product[field.name]}
-						onChange={handleChange}/>
-				))}
-				<Input variant='underlined' type='file' isRequired label='Imagen' placeholder='Imagen' name='image' onChange={handleChange}/>
+				<Input
+					variant='underlined'
+					isRequired
+					label='Nombre'
+					placeholder='Ingrese un nombre de producto'
+					name='name'
+					value={product.name}
+					onChange={handleChange}/>
+
+				<CategorySelect
+					product={product}
+					setProduct={setProduct}
+					categories={categories}
+					setCategory={setCategory}
+					categoryInput={categoryInput}
+					setCategoryInput={setCategoryInput}/>
+
+				<Input
+					variant='underlined'
+					label='Descripción'
+					placeholder='Ingrese una descripción de producto'
+					name='description'
+					value={product.description}
+					onChange={handleChange}/>
+
+				<Input
+					variant='underlined'
+					isRequired
+					label='Precio'
+					placeholder='Ingrese precio del producto'
+					name='price'
+					value={product.price}
+					onChange={handleChange}/>
+
+				<Input 
+					variant='underlined' 
+					type='file' 
+					isRequired 
+					label='Imagen' 
+					placeholder='Imagen' 
+					name='image' 
+					onChange={handleChange}/>
+				
 				<SubmitButton loading={loading} isDisabled={isDisabled} title='Create Product' />
 			</form>
 		</div>
