187a0dbf1b89270c93072b3246998e8bcd4a9fbf
Uriel Campos <urielcampos448@gmail.com>
2023-11-22 13:58:30 -0300

    feat[detail component]: added styles

--- components/detail/Detail.tsx
+++ components/detail/Detail.tsx
@@ -1,25 +1,26 @@
 'use client'
-import { Skeleton } from "@nextui-org/react"
-import { Image } from "@nextui-org/react"
-const Detail = ({ product }: { product: any}) => {
-
-    return (
-        <div>
-            <Skeleton
-            isLoaded = {product}
-            >
-                <h1>{product.name}</h1>
-                <Image
-                    src={product.image}
-                    alt={product.name}
-                    width={200}
-                    height={200}
-                />
-                <p>{product.description}</p>
-                <p>{product.price}</p>
-            </Skeleton>
-        </div>
-    )
+import { Skeleton, Image, Card, CardBody } from '@nextui-org/react'
+import Price from '../Price'
+const Detail = ({ product }: { product: any }): React.JSX.Element => {
+	return (
+		<Card >
+			<CardBody className='grid place-items-center'>
+				<Skeleton isLoaded={product.name}>
+					<h1>{product.name}</h1>
+				</Skeleton>
+				<Image
+					src={product.image}
+					alt={product.name}
+					width={200}
+					height={200}
+				/>
+				<Skeleton isLoaded={product.description}>
+					<p>{product.description}</p>
+				</Skeleton>
+				<Price className='text-2xl' price={product.price}/>
+			</CardBody>
+		</Card>
+	)
 }
 
 export default Detail
