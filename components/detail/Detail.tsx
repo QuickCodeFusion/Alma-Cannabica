67eb5ee419f3b16e392e73990819f9cd1ea7b74e
Uriel Campos <urielcampos448@gmail.com>
2023-11-22 15:20:08 -0300

    feat: responsive detail

--- components/detail/Detail.tsx
+++ components/detail/Detail.tsx
@@ -1,23 +1,31 @@
 'use client'
 import { Skeleton, Image, Card, CardBody } from '@nextui-org/react'
 import Price from '../Price'
+import Categories from '../Categories'
 const Detail = ({ product }: { product: any }): React.JSX.Element => {
+	const { name, price, image, category, description } = product
 	return (
 		<Card >
-			<CardBody className='grid place-items-center'>
-				<Skeleton isLoaded={product.name}>
-					<h1>{product.name}</h1>
+			<CardBody className='grid place-items-center gap-4'>
+				<Skeleton isLoaded={name}>
+					<h1>{name}</h1>
 				</Skeleton>
 				<Image
-					src={product.image}
-					alt={product.name}
+					src={image}
+					alt={name}
 					width={200}
 					height={200}
 				/>
-				<Skeleton isLoaded={product.description}>
-					<p>{product.description}</p>
+				<Skeleton isLoaded={category[0]} className='rounded-md'>
+					<Categories
+						key={category}
+						categories={category}
+					/>
 				</Skeleton>
-				<Price className='text-2xl' price={product.price}/>
+				<Skeleton isLoaded={description}>
+					<p>{description}</p>
+				</Skeleton>
+				<Price className='text-2xl' price={price}/>
 			</CardBody>
 		</Card>
 	)
