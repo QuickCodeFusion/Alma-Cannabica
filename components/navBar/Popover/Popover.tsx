cab21a5a140d91cfa598685d08122b296f01a94c
Davian Pabon <wilsondavianpa1998@outlook.com>
2023-11-22 07:48:04 -0500

    Altair/correction and cards (#35)
    * correction
    
    * correction
    
    * update cart
    
    * update searchBar

--- components/navBar/Popover/Popover.tsx
+++ components/navBar/Popover/Popover.tsx
@@ -1,19 +1,17 @@
 import { Popover, PopoverTrigger, PopoverContent, Button, Input } from "@nextui-org/react";
 import Image from 'next/image'
-export default function App() {
+import Cart from "@/components/cart/Cart";
+export const PopoverComponent = () => {
 
     const content = (
         <PopoverContent className="w-[240px]">
             {(titleProps) => (
                 <div className="px-1 py-2 w-full">
                     <p className="text-small font-bold text-foreground" {...titleProps}>
-                        Dimensions
+                        Productos
                     </p>
                     <div className="mt-2 flex flex-col gap-2 w-full">
-                        <Input defaultValue="100%" label="Width" size="sm" variant="bordered" />
-                        <Input defaultValue="300px" label="Max. width" size="sm" variant="bordered" />
-                        <Input defaultValue="24px" label="Height" size="sm" variant="bordered" />
-                        <Input defaultValue="30px" label="Max. height" size="sm" variant="bordered" />
+                        <Cart/>
                     </div>
                 </div>
             )}
