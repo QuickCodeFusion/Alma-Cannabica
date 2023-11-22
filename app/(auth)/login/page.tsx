3910c896f163dcd1e879f1869c3160bd11f7664c
Juan Diaz <juanjoc2000@hotmail.com>
2023-11-22 01:02:08 -0300

    fix[login]url

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -2,7 +2,7 @@
 import Login from '@/components/form/login/Login'
 import Register from '@/components/form/register/Register'
 import { Card, CardBody, Tab, Tabs } from '@nextui-org/react'
-import style from '@/components/form/login/Login.module.css'
+import style from '@/components/form/login/login.module.css'
 
 const LoginPage = (): React.JSX.Element => {
 	return (
