9924cfc677a32bad8d5d62d53da4c86511e8cced
Uriel Campos <urielcampos448@gmail.com>
2023-11-24 17:10:28 -0300

    feat: Added addToCartButton component

---
+++ components/button/addToCartButton.tsx
@@ -0,0 +1,34 @@
+'use client'
+import { addToCart } from '@/redux/feature/cartSlice'
+import { useDispatch } from '@/redux/hooks'
+import { type CardProduct, type Product } from '@/types/Product/type'
+import { Button } from '@nextui-org/react'
+
+interface props {
+	product: Product | CardProduct
+}
+
+const AddToCartButton: React.FC<props> = ({ product }): React.JSX.Element => {
+	const dispatch = useDispatch()
+	const handleClick = (): void => {
+		dispatch(addToCart(product))
+		console.log('AÑADI ', product, 'AL CARRITO')
+	}
+
+	return (
+		<>
+			<Button
+				onClick={handleClick}
+				isIconOnly
+				color="success"
+				variant="bordered"
+			>
+				<p className="scale-150 font-bold mb-1">
+                    +
+				</p>
+			</Button>
+		</>
+	)
+}
+
+export default AddToCartButton
