511c56fea3654c2fcf456f6308fb20f42ac3d978
Uriel Campos <urielcampos448@gmail.com>
2023-11-27 00:15:49 -0300

    feat[addToCartButton] added mutation

--- components/button/addToCartButton.tsx
+++ components/button/addToCartButton.tsx
@@ -1,8 +1,11 @@
 'use client'
+import { useUserSession } from '@/app/userContext'
 import { addToCart } from '@/redux/feature/cartSlice'
 import { useDispatch } from '@/redux/hooks'
+import { useAddToCartMutation } from '@/redux/service/cartAPI'
 import { type CardProduct, type Product } from '@/types/Product/type'
 import { Button } from '@nextui-org/react'
+import { toast } from 'sonner'
 
 interface props {
 	product: Product | CardProduct
@@ -10,8 +13,22 @@ interface props {
 
 const AddToCartButton: React.FC<props> = ({ product }): React.JSX.Element => {
 	const dispatch = useDispatch()
+	const [addToCartMutation] = useAddToCartMutation()
+	const { userSession } = useUserSession()
 	const handleClick = (): void => {
 		dispatch(addToCart(product))
+		addToCartMutation({
+			userId: userSession?.uid ?? '',
+			itemId: product.itemId,
+			value: 'add'
+		})
+			.then(() => {
+				toast.success('Agregado al carrito')
+			})
+			.catch((error) => {
+				console.error(error)
+				toast.error('Error al agregar al carrito')
+			})
 	}
 
 	return (
