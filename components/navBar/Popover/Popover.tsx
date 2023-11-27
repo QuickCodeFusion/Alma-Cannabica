9c8524588ea6bb3e73c5eb52f29aebd0266e3372
Uriel Campos <urielcampos448@gmail.com>
2023-11-27 13:50:25 -0300

    feat[Popover]: Added clearCart button

--- components/navBar/Popover/Popover.tsx
+++ components/navBar/Popover/Popover.tsx
@@ -1,13 +1,35 @@
 'use client'
-import { Popover, PopoverTrigger, PopoverContent, Button, Badge } from '@nextui-org/react'
+import { Popover, PopoverTrigger, PopoverContent, Button, Badge, useDisclosure } from '@nextui-org/react'
 import Image from 'next/image'
 import Cart from '@/components/cart/Cart'
 import { useEffect, useState } from 'react'
-import { useSelector } from '@/redux/hooks'
+import { useDispatch, useSelector } from '@/redux/hooks'
 import { type CartProduct } from '@/types/User/types'
+import { useUserSession } from '@/app/userContext'
+import { useClearCartMutation } from '@/redux/service/cartAPI'
+import { clearCart } from '@/redux/feature/cartSlice'
+import { toast } from 'sonner'
+import ConfirmModal from '@/components/modal/confirmModal'
 export const PopoverComponent = (): React.JSX.Element => {
 	const [itemCount, setItemCount] = useState(0)
 	const products: CartProduct[] = useSelector((state: any) => state.cart.cart)
+	const dispatch = useDispatch()
+	const [clearCartMutate] = useClearCartMutation()
+	const { userSession } = useUserSession()
+	const { isOpen, onOpen, onOpenChange } = useDisclosure()
+
+	const removeCart = (userId: string): void => {
+		dispatch(clearCart())
+		clearCartMutate(userId)
+			.then(() => {
+				toast.success('Se vació el carrito')
+			})
+			.catch((error: any) => {
+				console.error(error)
+				toast.error('Error al vaciar el carrito')
+			})
+	}
+
 	useEffect(() => {
 		setItemCount(products.reduce((acc: number, product: CartProduct) => acc + product.quantity, 0))
 	}, [products])
@@ -15,12 +37,28 @@ export const PopoverComponent = (): React.JSX.Element => {
 		<PopoverContent className="w-auto">
 			{(titleProps) => (
 				<div className="px-1 py-2 w-full">
-					<p className="text-small font-bold text-foreground" {...titleProps}>
+					<span className="flex justify-between items-center gap-4">
+						<h1 className="text-small font-bold text-foreground" {...titleProps}>
                         Carrito de compras
-					</p>
+						</h1>
+						<Button
+							size="sm"
+							color='danger'
+							onClick={onOpen}
+						>
+						Vaciar
+						</Button>
+					</span>
+
 					<div className="flex flex-col gap-1 w-full max-h-[70vh] p-0">
 						<Cart products={products}/>
 					</div>
+					<ConfirmModal
+						isOpen={isOpen}
+						onOpenChange={onOpenChange}
+						removeCart={removeCart}
+						userId={userSession?.uid ?? 'guest'}
+					/>
 				</div>
 			)}
 		</PopoverContent>
@@ -36,7 +74,7 @@ export const PopoverComponent = (): React.JSX.Element => {
 				showArrow
 				offset={10}
 				placement="bottom"
-				backdrop="blur"
+				backdrop={isOpen ? 'transparent' : 'blur'}
 			>
 				<PopoverTrigger>
 					<Button size="sm" isIconOnly className="bg-white border">
