5904e14cc92637124c083fdbc3899406cc089d74
Uriel Campos <urielcampos448@gmail.com>
2023-11-29 13:45:26 -0300

    fix no nested button warning

--- components/card/Card.tsx
+++ components/card/Card.tsx
@@ -25,7 +25,7 @@ const Card = ({ product }: { product: CardProduct }): React.JSX.Element => {
 				/>
 
 				<CardFooter className={style.cardFooter}>
-					<b className={style.nameProduct}>{name}</b>
+					<span className={style.nameProduct}>{name}</span>
 					
 						
 						<div className={style.containerBtn}>
