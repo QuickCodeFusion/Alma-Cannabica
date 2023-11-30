8fff8b68dcc45b58e4d45026d345d2d6cbe5fc3e
Uriel Campos <urielcampos448@gmail.com>
2023-11-30 11:06:29 -0300

    migrated searchbar to NextUI components

--- components/navBar/SearchBar/SearchBar.tsx
+++ components/navBar/SearchBar/SearchBar.tsx
@@ -6,6 +6,7 @@ import { useDispatch } from '@/redux/hooks'
 import { loadProducts } from '@/redux/feature/productsSlice'
 import { useGetFiltersQuery } from '@/redux/service/productsFilterAPI'
 import { useState, useEffect } from 'react'
+import { Button, Input } from '@nextui-org/react'
 
 const SearchBar = (): React.JSX.Element => {
 	const dispatch = useDispatch()
@@ -18,10 +19,6 @@ const SearchBar = (): React.JSX.Element => {
 	}, [products])
 
 	console.log(products)
-	const handleChange = (event: React.ChangeEvent<HTMLInputElement>): any => {
-		const value = event.target.value
-		setInput(value)
-	}
 
 	const handleSubmit = (): any => {
 		setNameProduct(input)
@@ -30,10 +27,34 @@ const SearchBar = (): React.JSX.Element => {
 
 	return (
 		<>
-			<input type="text" onChange={handleChange} className={style.input} />
-			<button className={style.button} onClick={handleSubmit}>
-				<Image src="/buscar.png" alt="Buscar" width={20} height={20}></Image>
-			</button>
+			<Input placeholder='Busca productos' radius='full' onValueChange={setInput} size='sm' classNames={{
+				input: [
+					'text-black',
+					'placeholder:text-black/50'
+				],
+				inputWrapper: [
+					'shadow-md',
+					'bg-gray-300',
+					'group-data-[focus=true]:bg-slate-200',
+					'!cursor-text',
+					'group-data-[hover=true]:bg-gray-400/60'
+				],
+				base: 'w-1/2 data-[focus=true]:w-full transition-all duration-500'
+			}}/>
+			<Button
+				onClick={handleSubmit}
+				color='default'
+				className='bg-transparent'
+				isIconOnly
+				size='sm'
+			>
+				<Image
+					src='/buscar.png'
+					alt='botón de búsqueda'
+					width={20}
+					height={20}
+				/>
+			</Button>
 		</>
 	)
 }
