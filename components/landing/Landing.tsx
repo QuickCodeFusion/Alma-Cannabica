36fa0bc6126fad418cbfda19497615ff48520012
Uriel Campos <urielcampos448@gmail.com>
2023-12-06 04:41:54 -0300

    Styled Landing + Added Carousel

--- components/landing/Landing.tsx
+++ components/landing/Landing.tsx
@@ -1,20 +1,12 @@
 'use client'
-import style from './landing.module.css'
-import Loading from '@/app/loading'
-import { Card, CardFooter, Image, Button } from '@nextui-org/react'
-import { Swiper, SwiperSlide } from 'swiper/react'
-import 'swiper/css'
-import 'swiper/css/effect-coverflow'
-import 'swiper/css/pagination'
-import 'swiper/css/navigation'
-import { EffectCoverflow, Pagination, Navigation, Autoplay } from 'swiper/modules'
+import { Card, Button } from '@nextui-org/react'
 import { useGetCarouselQuery } from '@/redux/service/carouselAPI'
-import { useDispatch, useSelector } from '@/redux/hooks'
 import Link from 'next/link'
 import React from 'react'
+import LandingCarousel from './LandingCarousel'
 
-const Landing = () => {
-	const { data, error, isLoading } = useGetCarouselQuery(null)
+const Landing = (): React.JSX.Element => {
+	const { data, isLoading } = useGetCarouselQuery(null)
 
 	const frases = [
 		`La marihuana medicinal se puede utilizar para: Aliviar el dolor. \n
@@ -26,10 +18,10 @@ const Landing = () => {
 	const randomFrase = frases[randomIndex]
 
 	return (
-		<div className='flex justify-center items-center my-24'>
-			<Card className='w-1/2 p-10 text-green-600 font-medium text-2xl text-center shadow-2xl border drop-shadow-2xl '>
-				<h1 className='text-5xl font-semibold my-6'>¿Sabias que...?</h1>
+		<div className='flex justify-center items-center my-12 w-full'>
+			<Card className='w-2/3 p-10 text-green-600 font-medium text-2xl text-center shadow-2xl border drop-shadow-2xl gap-8'>
 				<div className='flex flex-col items-center gap-4'>
+					<h1 className='text-5xl font-semibold my-6'>¿Sabias que...?</h1>
 					<p className='text-center'>
 						{randomFrase.split('\n').map((line, lineIndex) => (
 							<React.Fragment key={lineIndex}>
@@ -44,12 +36,17 @@ const Landing = () => {
 						size="lg"
 						as={Link}
 						href='/products'
-						className='w-1/4 font-semibold bg-green-400 bg-opacity-75 text-black/90 hover:bg-green-500 hover:bg-opacity-80 duration-300'
+						className='
+						w-1/4
+						font-semibold
+						bg-green-400 bg-opacity-75 hover:bg-green-500 hover:bg-opacity-80
+						text-black/90
+						duration-300'
 					>
 						Ver tienda
 					</Button>
 				</div>
-				
+				<LandingCarousel products={data ?? []} isLoading={isLoading}/>
 			</Card>
 		</div>
 	)
