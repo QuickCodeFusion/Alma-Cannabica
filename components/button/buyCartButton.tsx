9c2968d936f51ba1c18fff3d05f4fed953b3529a
UrielC2004 <131821174+UFCampos@users.noreply.github.com>
2023-12-13 18:57:24 -0300

    Uriel/mpcheckout (#49)
    * rewrote Landing to use tailwind classes
    
    * feat[globals.css]: add import for slick carousel
    
    * feat[Card FC]: Add support for custom classnames
    
    * feat[landing]: Added LandingCarousel component
    
    * Styled Landing + Added Carousel
    
    * uninstalled Swiper in favor of Slick Carousel
    
    * Added Arrow icon
    
    * updated Arrow to accept custom classNames
    
    * updated Arrow icon
    
    * finished landing styles
    
    * [package.json]: react-slick types missing
    
    * fixed Slider types attributes
    
    * adjusted cards + carousel
    
    * fixed carousel route to include itemId
    
    * Card now renders div instead of button
    
    * menu now closes after pressing an item
    
    * installed mercadopago
    
    * added basic checkout API endpoint
    
    * checkout backend finished
    
    * feat[app]: Added checkout page
    
    * completed checkout backend
    
    * feat[BuyModel]: added checkout to single product
    
    * feat[utils]: added checkout functions
    
    * feat[BuyModal|BuyCartButton] added checkout url
    
    * Replaced Whatsapp icon with MercadoPago
    
    * fix
    
    * fixed icon size

--- components/button/buyCartButton.tsx
+++ components/button/buyCartButton.tsx
@@ -1,23 +1,39 @@
 'use client'
 import { type CartProduct } from '@/types/User/types'
 import { Button, Link } from '@nextui-org/react'
-import Whatsapp from '../icons/Whatsapp'
+import { useEffect, useState } from 'react'
+import { getPreferenceUrl } from '@/utils/checkoutUtils'
+import { toast } from 'sonner'
 
 interface props {
 	products: CartProduct[]
 }
 
 const BuyCartButton: React.FC<props> = ({ products }): React.JSX.Element => {
+	const [preferenceUrl, SetPreferenceUrl] = useState<string>('')
+
+	useEffect(() => {
+		if (products) {
+			getPreferenceUrl(products)
+				.then((url) => {
+					SetPreferenceUrl(url)
+				})
+				.catch((error) => {
+					console.error(error)
+					toast.error(error.message)
+				})
+		}
+	}, [products])
+
 	return (
 		<Button
 			isExternal
 			showAnchorIcon
-			anchorIcon={<Whatsapp/>}
 			color='success'
 			as={Link}
-			href={'https://youtu.be/dQw4w9WgXcQ'}
+			href={preferenceUrl}
 			className='text-white'
-		>Consultar </Button>
+		>Comprar con Mercado Pago</Button>
 	)
 }
 
