04e6372dc8536cbfd914249d4d799b469d878015
Uriel Campos <urielcampos448@gmail.com>
2023-12-13 20:46:03 -0300

    feat[/products] RTKQuery is now done by page to avoid useless fetch

--- components/cards/Cards.tsx
+++ components/cards/Cards.tsx
@@ -1,11 +1,8 @@
 'use client'
 import Card from '../card/Card'
 import Loading from '@/app/loading'
-import { useSelector } from '@/redux/hooks'
 
-const Cards = (): React.JSX.Element => {
-	const { products, isLoading, isError } = useSelector((state: any) => state.products)
-
+const Cards = ({ products, isLoading, isError }: { products: any, isLoading: boolean, isError: boolean }): React.JSX.Element => {
 	return (
 		<div className='md:grid md:grid-cols-3 md:grid-rows-3 md:gap-16 md:my-10'>
 			{isError && <div>Error</div>}
--- components/products/Products.tsx
+++ components/products/Products.tsx
@@ -1,13 +1,31 @@
 'use client'
+import { useSelector } from '@/redux/hooks'
 import Cards from '../cards/Cards'
 import FiltersContainer from '../filters/FiltersContainer'
+import { useGetFiltersQuery } from '@/redux/service/productsFilterAPI'
+import { useEffect, useState } from 'react'
 
 const Products = (): JSX.Element => {
+	const query = useSelector((state: any) => state.searchBar.query)
+
+	const [products, setProducts] = useState([])
+
+	const { data, isLoading, isError } = useGetFiltersQuery(query)
+
+	useEffect(() => {
+		data && setProducts(data)
+	}, [data])
+
 	return (
 		<div className='flex flex-col md:flex-row md:items-start md:justify-center md:gap-12 w-full'>
 			<FiltersContainer />
 			<>
-				<Cards />
+				{!isLoading && !isError && products.length === 0 && (
+					<div className='flex flex-col items-center justify-center w-full h-full'>
+						<h1 className='text-3xl font-bold'>No hay resultados</h1>
+					</div>
+				)}
+				<Cards products={products} isLoading={isLoading} isError={isError}/>
 			</>
 		</div>
 	)
