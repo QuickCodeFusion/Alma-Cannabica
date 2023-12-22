88034af29e3f0c5dfba9658d85c5613aabcf547b
juanpo12 <juanjoc2000@hotmail.com>
2023-12-22 02:58:47 -0300

    feat:Added page

---
+++ app/forgot-password/page.tsx
@@ -0,0 +1,11 @@
+import RecoverPassword from '@/components/form/recoverPassword/RecoverPassword'
+
+const Forgotpassword = (): JSX.Element => {
+	return (
+		<div>
+			<RecoverPassword />
+		</div>
+	)
+}
+
+export default Forgotpassword
