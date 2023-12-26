2dd9aee0ac3df52791f26b10d75edaa8880838f0
Uriel Campos <urielcampos448@gmail.com>
2023-12-26 20:48:10 -0300

    temp pagination return

--- components/pagination/Pagination.tsx
+++ components/pagination/Pagination.tsx
@@ -1,21 +1,8 @@
 'use client'
-import { useDispatch } from '@/redux/hooks'
 
 const Pagination = (): React.JSX.Element => {
-	const dispatch = useDispatch()
-
-	const changePage = (page) => {
-		if (page === 'derc') {
-		} else {
-		}
-	}
 	return (
-		<div>
-			<button onClick={() => {
-				changePage('izq')
-			}}> izq </button>
-			<button onClick={ () => { changePage('derc') }}> derc </button>
-		</div>
+		<p>Esto es el paginado</p>
 	)
 }
 
