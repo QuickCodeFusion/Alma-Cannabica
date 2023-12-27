9b918c94996a36553cb1724175c8847a5dc777a6
UrielC2004 <131821174+UFCampos@users.noreply.github.com>
2023-12-27 15:46:49 -0300

    Altair/admin product (#54)
    * table
    
    * table update
    
    * table modal
    
    * table modal update
    
    * table modal update
    
    * table modal update
    
    * table update
    
    * table update
    
    * table Edit product
    
    * table Edit product
    
    * edit product
    
    * condicional button
    
    * feat[api/carrousel/route] POST now copies product instead of creating a new one
    
    * product now has a new attribute when added to carousel
    
    * fase de prueba no tocar
    
    * btn ready
    
    * refactor product types to follow DRY
    
    * refactor[api/carrousel] to not include inCarousel attribute in collection
    DELETE Method now also changes inCarousel attribute to false in products collection
    
    * feat[Sidebar]: AdmProducts is now lazy loaded
    
    * feat[/products] RTKQuery is now done by page
    to avoid useless fetch
    
    * update ready
    
    * update ready
    
    * fix admproducts types & unnecessary map
    
    * feat[api/products] POST now parses price to int
    
    * feat[api/backPaginator] now re-uses productsQuery
    
    * BREAKING: renamed backPaginator to prevPaginator
    
    * fix[admProducts] type error on category.map
    
    * temp pagination return
    
    ---------
    
    Co-authored-by: altair201 <wilsondavianpa1998@outlook.com>

--- app/api/filters/backPaginator.ts
+++ app/api/filters/backPaginator.ts
@@ -1,29 +1,34 @@
 import { db } from '@/firebase/config'
 import { type Product } from '@/types/Product/type'
-import { collection, doc, getDoc, getDocs, orderBy, query } from 'firebase/firestore'
+import { type Query, doc, getDoc, getDocs, type DocumentSnapshot, type DocumentData } from 'firebase/firestore'
 
 /**
- * Retrieves the previous product based on the provided first product ID.
+ * Generates a paginated result by going back from a given product ID.
  *
- * @param {string} firstProductId - The ID of the first product.
- * @return {Promise<any>} - A promise that resolves with the previous product.
+ * @param {string} firstProductId - The ID of the first product to start pagination from.
+ * @param {Query} productsQuery - The query used to fetch the products.
+ * @return {Promise<string>} - A promise that resolves to the paginated result.
  */
-export const backPaginator = async (firstProductId: string): Promise<any> => {
-	if (firstProductId !== '') {
-		const productRef = query(collection(db, 'products'), orderBy('name', 'asc'))
-		const productsSnapshot = await getDocs(productRef)
-		const products: Product[] = productsSnapshot.docs.map((doc) => ({
-			...(doc.data() as Product),
-			itemId: doc.id
-		}))
-
-		const index = products.findIndex((product) => product.itemId === firstProductId)
-
-		if (index !== -1 && index >= 2) {
-			const productId = products[index - 2].itemId
-			return productId && await getDoc(doc(db, 'products', productId))
-		}
-
-		return products[0].itemId
+export const prevPaginator = async (firstProductId: string, productsQuery: Query): Promise<string | DocumentSnapshot<DocumentData, DocumentData>> => {
+	if (firstProductId === '') {
+		throw new Error('First product ID is required')
 	}
+	if (!productsQuery) {
+		throw new Error('Products query is required')
+	}
+
+	const productsSnapshot = await getDocs(productsQuery)
+	const products: Product[] = productsSnapshot.docs.map((doc) => ({
+		...(doc.data() as Product),
+		itemId: doc.id
+	}))
+
+	const index = products.findIndex((product) => product.itemId === firstProductId)
+
+	if (index >= 2) {
+		const productId = products[index - 2].itemId
+		return productId && (await getDoc(doc(db, 'products', productId)))
+	}
+
+	return products[0]?.itemId
 }
--- app/api/filters/filters.ts
+++ app/api/filters/filters.ts
@@ -1,45 +1,44 @@
 import { db } from '@/firebase/config'
 import { type Product } from '@/types/Product/type'
 import { query, where, collection, getDocs, orderBy, limit, startAfter, doc, getDoc, endBefore, startAt } from 'firebase/firestore'
-import { backPaginator } from './backPaginator'
+import { prevPaginator } from './backPaginator'
 
 export const filters = async (name: string, minPrice: string, maxPrice: string, category: string, order: string, firstProductId: string, lastProductId: string): Promise<Product[]> => {
-	let productRef = query(collection(db, 'products'), orderBy('name', 'asc'), limit(6))
-
-	if (lastProductId !== '') {
-		const lastProductRef = await getDoc(doc(db, 'products', lastProductId))
-		productRef = query(productRef, startAfter(lastProductRef), limit(2))
-	}
-
-	if (firstProductId !== '') {
-		const firstProductRef = await getDoc(doc(db, 'products', firstProductId))
-		const firstProductRef2 = await backPaginator(firstProductId)
-		productRef = query(productRef, startAt(firstProductRef2), endBefore(firstProductRef), limit(2))
-	}
+	let productsQuery = query(collection(db, 'products'), limit(6), orderBy('price', 'asc'))
 
 	if (category !== '') {
-		productRef = query(productRef, where('category', '==', category))
+		const categories = Array.isArray(category) ? category : [category]
+		productsQuery = query(productsQuery, where('category', 'array-contains-any', categories))
 	}
 
-	if (minPrice !== '') {
-		productRef = query(productRef, where('price', '>=', parseInt(minPrice)))
-	}
-
-	if (maxPrice !== '') {
-		productRef = query(productRef, where('price', '<=', parseInt(maxPrice)))
+	if (minPrice !== '' || maxPrice !== '') {
+		const min = parseInt(minPrice) - 1 || 1
+		const max = parseInt(maxPrice) + 1 || 1000000
+		productsQuery = query(productsQuery, where('price', '>=', min), where('price', '<=', max))
 	}
 
 	switch (order) {
 		case 'low':
-			productRef = query(productRef, orderBy('price', 'asc'))
+			productsQuery = query(productsQuery, orderBy('price', 'asc'))
 			break
 
 		case 'high':
-			productRef = query(productRef, orderBy('price', 'desc'))
+			productsQuery = query(productsQuery, orderBy('price', 'desc'))
 			break
 	}
 
-	const productsSnapshot = await getDocs(productRef)
+	if (lastProductId !== '') {
+		const lastProductRef = await getDoc(doc(db, 'products', lastProductId))
+		productsQuery = query(productsQuery, startAfter(lastProductRef), limit(2))
+	}
+
+	if (firstProductId !== '') {
+		const firstProductRef = await getDoc(doc(db, 'products', firstProductId))
+		const firstProductRef2 = await prevPaginator(firstProductId, productsQuery)
+		productsQuery = query(productsQuery, startAt(firstProductRef2), endBefore(firstProductRef), limit(2))
+	}
+
+	const productsSnapshot = await getDocs(productsQuery)
 	const products: Product[] = productsSnapshot.docs.map((doc) => ({
 		...(doc.data() as Product),
 		itemId: doc.id
