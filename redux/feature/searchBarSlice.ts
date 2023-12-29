e62573aada02ae96486b6c0591b1cfbed4dac2e4
Uriel Campos <urielcampos448@gmail.com>
2023-12-29 14:15:24 -0300

    feat[redux/searchBar] added pagination query

--- redux/feature/searchBarSlice.ts
+++ redux/feature/searchBarSlice.ts
@@ -7,6 +7,8 @@ export interface queryState {
 		name: string
 		minPrice: string
 		maxPrice: string
+		firstProductId: string
+		lastProductId: string
 	}
 }
 
@@ -16,7 +18,9 @@ const initialState: queryState = {
 		order: '',
 		name: '',
 		minPrice: '',
-		maxPrice: ''
+		maxPrice: '',
+		firstProductId: '',
+		lastProductId: ''
 	}
 }
 
