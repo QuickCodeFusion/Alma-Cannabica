f1f54706865c2b96dbab3c441a8040ba9362ad6d
Uriel Campos <urielcampos448@gmail.com>
2023-12-30 01:14:19 -0300

    feat[Footer FC] migrate to tailwind classes and retouch styles

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,34 +1,35 @@
-import style from './footer.module.css'
 import { LogoInstagram, LogoWhat } from './LogoSocial'
 import { Tooltip } from '@nextui-org/react'
 import logo from '@/public/logo.png'
-const Footer = () => {
+import Link from 'next/link'
+
+const Footer = (): JSX.Element => {
 	return (
-		<div className={style.container}>
-			<div className={style.info}>
+		<div className='flex flex-wrap justify-between mt-2 py-4 px-4 w-full bg-white shadow-small'>
+			<div className='flex flex-wrap gap-8 justify-between'>
 				<div>
-				<img src={logo.src} alt='logo' width={50} />
+					<img src={logo.src} alt='logo' width={50} />
 				</div>
 				<div>
 					<p className='text-sm'>Alma Canabica</p>
-					<p className='text-xs'> San Juan, Argentina</p>
+					<p className='text-xs'> Buenos Aires, Argentina</p>
 					<p className='text-xs'>© 2023. Todos los derechos reservados.</p>
 				</div>
-				<div>
+				<div className='flex flex-col gap-0'>
 					<p className='text-sm'>Contacto</p>
 					<p className='text-xs'>+54 9 11 2793-8262</p>
-					<p className='text-xs'>almacannabicawork@gmail.com</p>
+					<Link className='text-xs' href='mailto:almacannabicawork@gmail.com'>almacannabicawork@gmail.com</Link>
 				</div>
 			</div>
-			<div className={style.logo}>
-				<div className={style.LogoInstagram}>
+			<div className='flex w-1/12 justify-around items-center'>
+				<div className='rounded-full h-fit p-1 bg-gray-300/70 hover:bg-fuchsia-600 hover:shadow-black LogoInstagram'>
 					<Tooltip offset={15} content='Instagram'>
-						<a href="https://www.instagram.com/lauramarielacevedo?igsh=M2VmaHloZXdnbDNs" className='rounded-full'><LogoInstagram  /></a>
+						<Link href="https://www.instagram.com/lauramarielacevedo?igsh=M2VmaHloZXdnbDNs" className='rounded-full'><LogoInstagram /></Link>
 					</Tooltip>
 				</div>
-				<div className={style.LogoWhat}>
+				<div className='rounded-full h-fit p-1 bg-gray-300/70 hover:bg-green-500 hover:shadow-black'>
 					<Tooltip offset={15} content='Whatsapp'>
-						<a href="https://wa.me/c/5491127938262"><LogoWhat /></a>
+						<Link href="https://wa.me/c/5491127938262"><LogoWhat /></Link>
 					</Tooltip>
 				</div>
 			</div>
