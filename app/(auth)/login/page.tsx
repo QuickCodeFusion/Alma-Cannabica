1cc77eb8ee65266b46ade690f2c0972542db13c2
Uriel Campos <urielcampos448@gmail.com>
2023-12-30 02:34:02 -0300

    feat[login page] migrated to tailwind classes

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -2,13 +2,12 @@
 import Login from '@/components/form/login/Login'
 import Register from '@/components/form/register/Register'
 import { Card, CardBody, Tab, Tabs } from '@nextui-org/react'
-import style from '@/components/form/login/login.module.css'
 
 const LoginPage = (): React.JSX.Element => {
 	return (
-		<div className={style.container}>
-			<Card className="max-w-full">
-				<CardBody className="overflow-hidden">
+		<div className='h-screen md:h-2/3 w-full flex flex-col justify-between items-center p-4'>
+			<Card className="w-full md:w-1/3">
+				<CardBody>
 					<Tabs className='justify-center' disableAnimation
 					>
 						<Tab key="login" title="Ingresar">
