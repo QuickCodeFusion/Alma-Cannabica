1a118c134038e212a1d2290532a6d3aba333e2fc
Uriel Campos <urielcampos448@gmail.com>
2023-12-30 02:50:39 -0300

    feat[RecoverPassword FC] input now supports enter key press

--- components/form/recoverPassword/RecoverPassword.tsx
+++ components/form/recoverPassword/RecoverPassword.tsx
@@ -8,20 +8,35 @@ import { toast } from 'sonner'
 const RecoverPassword = (): React.JSX.Element => {
 	const [email, setEmail] = useState('')
 
+	const handleSubmit = (email: string): void => {
+		sendPasswordResetEmail(auth, email)
+			.then(() => {
+				toast.success('El email para reestablecer la contraseña ha sido enviado')
+			})
+			.catch((error) => {
+				toast.error(error.message)
+			})
+	}
+
 	return (
-		<div>
-			<h1>RecoverPassword</h1>
-			<Input placeholder='Email' value={email} onValueChange={setEmail}></Input>
-			<Button onClick={() => {
-				sendPasswordResetEmail(auth, email)
-					.then(function () {
-						// Password reset email sent.
-						toast.success('Password reset email sent.')
-					})
-					.catch((error) => {
-						toast.error(error.message)
-					})
-			}}>Enviar</Button>
+		<div className='flex flex-col gap-4 text-center w-full h-fit border m-2 rounded-md shadow-medium p-6'>
+			<h1 className='text-xl md:text-2xl'>Reestablecer contraseña</h1>
+			<Input
+				placeholder='Ingrese el email de su cuenta'
+				value={email}
+				onValueChange={setEmail}
+				onKeyUp={(e) => {
+					if (e.key === 'Enter') {
+						handleSubmit(email)
+					}
+				}}
+			/>
+			<Button
+				className='self-center w-fit'
+				onClick={() => { handleSubmit(email) }}
+			>
+				Enviar
+			</Button>
 		</div>
 	)
 }
