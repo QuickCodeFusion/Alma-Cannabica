b44955808af4c819068f1004ad43206d01c7fb42
UrielC2004 <131821174+UFCampos@users.noreply.github.com>
2023-12-29 23:08:26 -0300

    Uriel/front pagination (#55)
    * table
    
    * table update
    
    * table modal
    
    * table modal update
    
    * table modal update
    
    * table modal update
    
    * table update
    
    * table update
    
    * table Edit product
    
    * table Edit product
    
    * edit product
    
    * condicional button
    
    * feat[api/carrousel/route] POST now copies product instead of creating a new one
    
    * product now has a new attribute when added to carousel
    
    * fase de prueba no tocar
    
    * btn ready
    
    * refactor product types to follow DRY
    
    * refactor[api/carrousel] to not include inCarousel attribute in collection
    DELETE Method now also changes inCarousel attribute to false in products collection
    
    * feat[Sidebar]: AdmProducts is now lazy loaded
    
    * feat[/products] RTKQuery is now done by page
    to avoid useless fetch
    
    * update ready
    
    * update ready
    
    * fix admproducts types & unnecessary map
    
    * feat[api/products] POST now parses price to int
    
    * feat[api/backPaginator] now re-uses productsQuery
    
    * BREAKING: renamed backPaginator to prevPaginator
    
    * fix[admProducts] type error on category.map
    
    * temp pagination return
    
    * feat[redux/searchBar] added pagination query
    
    * feat[searchBar FC] added Enter input
    
    * feat[redux/searchBar] Added pagination reducers
    
    * adjusted Cards to fit pagination component
    
    * adjusted Filters card to have a max w and margin
    
    * feat: Added functional pagination component
    
    * feat[Products FC] added pagination component and props
    
    ---------
    
    Co-authored-by: altair201 <wilsondavianpa1998@outlook.com>

--- components/products/Products.tsx
+++ components/products/Products.tsx
@@ -5,11 +5,12 @@ import FiltersContainer from '../filters/FiltersContainer'
 import Pagination from '../pagination/Pagination'
 import { useGetFiltersQuery } from '@/redux/service/productsFilterAPI'
 import { useEffect, useState } from 'react'
+import { type Product } from '@/types/Product/type'
 
 const Products = (): JSX.Element => {
 	const query = useSelector((state: any) => state.searchBar.query)
 
-	const [products, setProducts] = useState([])
+	const [products, setProducts] = useState<Product[]>([])
 
 	const { data, isLoading, isError } = useGetFiltersQuery(query)
 
@@ -18,17 +19,17 @@ const Products = (): JSX.Element => {
 	}, [data])
 
 	return (
-		<div className='flex flex-col md:flex-row md:items-start md:justify-center md:gap-12 w-full'>
+		<div className='flex flex-col md:flex-row md:items-start md:justify-center md:gap-1 w-full'>
 			<FiltersContainer />
-			<>
+			<div className='flex w-full flex-col my-0 md:w-2/3'>
 				{!isLoading && !isError && products.length === 0 && (
 					<div className='flex flex-col items-center justify-center w-full h-full'>
 						<h1 className='text-3xl font-bold'>No hay resultados</h1>
 					</div>
 				)}
 				<Cards products={products} isLoading={isLoading} isError={isError}/>
-			</>
-			<Pagination/>
+				<Pagination firstProductId={products[0]?.itemId} lastProductId={products[products.length - 1]?.itemId}/>
+			</div>
 
 		</div>
 	)
