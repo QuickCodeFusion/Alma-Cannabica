c5ee006f29d6c22e1d29889a376ef85428565c24
Uriel Campos <urielcampos448@gmail.com>
2024-01-03 10:55:51 -0300

    adjusted forgot-password height

--- app/forgot-password/page.tsx
+++ app/forgot-password/page.tsx
@@ -3,7 +3,7 @@ import RecoverPassword from '@/components/form/recoverPassword/RecoverPassword'
 
 const Forgotpassword = (): JSX.Element => {
 	return (
-		<main className='w-screen flex justify-center'>
+		<main className='w-screen h-3/4 flex justify-center items-center'>
 			<RecoverPassword />
 		</main>
 	)
