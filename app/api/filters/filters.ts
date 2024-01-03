57d31592185e8ea82cc67071612cef4c7fecf572
Uriel Campos <urielcampos448@gmail.com>
2024-01-03 13:27:19 -0300

    feat[api/filters] now has pageSize and default order + better pagination method

--- app/api/filters/filters.ts
+++ app/api/filters/filters.ts
@@ -1,10 +1,20 @@
 import { db } from '@/firebase/config'
 import { type Product } from '@/types/Product/type'
-import { query, where, collection, getDocs, orderBy, limit, startAfter, doc, getDoc, endBefore, startAt } from 'firebase/firestore'
-import { prevPaginator } from './backPaginator'
+import { query, where, collection, getDocs, orderBy, limit, startAfter, doc, getDoc, endBefore, limitToLast, getCountFromServer } from 'firebase/firestore'
 
-export const filters = async (name: string, minPrice: string, maxPrice: string, category: string, order: string, firstProductId: string, lastProductId: string): Promise<Product[]> => {
-	let productsQuery = query(collection(db, 'products'), limit(6), orderBy('price', 'asc'))
+export const filters = async (
+	name: string,
+	minPrice: string,
+	maxPrice: string,
+	category: string,
+	order: string,
+	firstProductId: string,
+	lastProductId: string,
+	pageSize: number = 6): Promise<{ products: Product[], totalPages: number }> => {
+	let productsQuery = query(collection(db, 'products'))
+
+	const totalDocs = await getCountFromServer(productsQuery)
+	const totalPages = Math.ceil(totalDocs.data().count / pageSize)
 
 	if (category !== '') {
 		const categories = Array.isArray(category) ? category : [category]
@@ -25,29 +35,42 @@ export const filters = async (name: string, minPrice: string, maxPrice: string, 
 		case 'high':
 			productsQuery = query(productsQuery, orderBy('price', 'desc'))
 			break
+
+		default:
+			productsQuery = query(productsQuery, orderBy('name', 'asc'))
+			break
+	}
+
+	if (lastProductId === '' && firstProductId === '') {
+		productsQuery = query(productsQuery, limit(pageSize))
 	}
 
 	if (lastProductId !== '') {
 		const lastProductRef = await getDoc(doc(db, 'products', lastProductId))
-		productsQuery = query(productsQuery, startAfter(lastProductRef), limit(2))
+		productsQuery = query(productsQuery, startAfter(lastProductRef), limit(pageSize))
 	}
 
 	if (firstProductId !== '') {
 		const firstProductRef = await getDoc(doc(db, 'products', firstProductId))
-		const firstProductRef2 = await prevPaginator(firstProductId, productsQuery)
-		productsQuery = query(productsQuery, startAt(firstProductRef2), endBefore(firstProductRef), limit(2))
+		productsQuery = query(productsQuery, endBefore(firstProductRef), limitToLast(pageSize))
 	}
 
 	const productsSnapshot = await getDocs(productsQuery)
-	const products: Product[] = productsSnapshot.docs.map((doc) => ({
+	let products: Product[] = productsSnapshot.docs.map((doc) => ({
 		...(doc.data() as Product),
 		itemId: doc.id
 	}))
 
 	if (name !== '') {
-		const data = products?.filter((product) => product.name.toLowerCase().includes(name.toLowerCase()))
-		return data
+		products = products?.filter((product) => product.name.toLowerCase().includes(name.toLowerCase()))
+		return {
+			products,
+			totalPages
+		}
 	}
 
-	return products
+	return {
+		products,
+		totalPages
+	}
 }
