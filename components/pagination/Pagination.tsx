3b2e5d11b18ed292ba017187b1ee34f0a4ba3deb
Uriel Campos <urielcampos448@gmail.com>
2024-01-03 13:51:56 -0300

    feat[pagination Fc] added loading and disabled states

--- components/pagination/Pagination.tsx
+++ components/pagination/Pagination.tsx
@@ -13,31 +13,40 @@ interface props {
 
 const Pagination: React.FC<props> = ({ firstProductId, lastProductId, totalPages }): React.JSX.Element => {
 	const [page, setPage] = useState(1)
+	const [isLoading, setIsLoading] = useState(false)
 	const dispatch = useDispatch()
 	const handlePageChange = (action: string): void => {
+		setIsLoading(true)
 		if (action === 'prev') {
 			dispatch(prevPageQuery(firstProductId))
 			setPage(page - 1)
+			setTimeout(() => { setIsLoading(false) }, 1000)
 		} else {
 			dispatch(nextPageQuery(lastProductId))
 			setPage(page + 1)
+			setTimeout(() => { setIsLoading(false) }, 1000)
 		}
 	}
 	return (
 		<>
 			<span className='hidden md:flex gap-4 my-2 justify-center'>
 				<Button
+					className='disabled:'
+					isDisabled={page === 1}
+					isLoading={isLoading}
 					color='success'
 					onClick={() => { handlePageChange('prev') }}
 				>
-				Anterior
+					{isLoading ? null : 'Anterior'}
 				</Button>
 				<span className='text-xl font-bold text-green-950'>{page} de {totalPages}</span>
 				<Button
+					isDisabled={page === totalPages}
 					color='success'
 					onClick={() => { handlePageChange('next') }}
+					isLoading={isLoading}
 				>
-				Siguiente
+					{isLoading ? null : 'Siguiente'}
 				</Button>
 			</span>
 			<Button
